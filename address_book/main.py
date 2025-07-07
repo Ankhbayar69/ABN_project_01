d8881e461e5442d72ef30fef30697d62a799f620
Zeus <zeus@admin-03s-iMac.local>
2025-07-07 14:09:33 +0800

    conflict solved

--- address_book/main.py
+++ address_book/main.py
@@ -1,17 +1,14 @@
-<<<<<<< HEAD
-contact = {"name": "Namjilsuren","phone": "99115090", "email": "namjilsuren5090@gmail.com"}
-contact = {
+
+contact1 = {"name": "Namjilsuren","phone": "99115090", "email": "namjilsuren5090@gmail.com"}
+contact2 = {
     "name": "Batotgon",
     "phone": "88008051",
     "email": "batotgon1111@gmail.com"
 }
-print(contact)
-=======
-contact = {"name": "Namjilsuren",
-           "phone": "99115090", 
-           "email": "namjilsuren5090@gmail.com",
+
+contact3 = {
            'name' : 'TS.Ankhbayar',
            'phone' : '99090105',
            'email' : 'mongoloo.soft@gmail.com'
             }
->>>>>>> fbda7e69432762b57efe2a2c2711f4a9dce03a0c
+
