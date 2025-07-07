4806d953af9049119c8b19e5cc0cc9009fe79e20
Zeus <zeus@ADMIN-02s-iMac.local>
2025-07-07 13:51:54 +0800

    contact

--- address_book/main.py
+++ address_book/main.py
@@ -1,1 +1,7 @@
 contact = {"name": "Namjilsuren","phone": "99115090", "email": "namjilsuren5090@gmail.com"}
+contact = {
+    "name": "Batotgon",
+    "phone": "88008051",
+    "email": "batotgon1111@gmail.com"
+}
+print(contact)
