fbda7e69432762b57efe2a2c2711f4a9dce03a0c
Zeus <zeus@admin-01s-iMac.local>
2025-07-07 13:50:18 +0800

    add: address_book

--- address_book/main.py
+++ address_book/main.py
@@ -1,1 +1,7 @@
-contact = {"name": "Namjilsuren","phone": "99115090", "email": "namjilsuren5090@gmail.com"}
+contact = {"name": "Namjilsuren",
+           "phone": "99115090", 
+           "email": "namjilsuren5090@gmail.com",
+           'name' : 'TS.Ankhbayar',
+           'phone' : '99090105',
+           'email' : 'mongoloo.soft@gmail.com'
+            }
